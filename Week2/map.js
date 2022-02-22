0f5717f4b79911f10eecfaf04d0d7f41a0a07b99
Nutcracko <unalbatuhan@ancorax.com>
2022-02-22 14:08:18 +0300

    small difference

--- Week2/map.js
+++ Week2/map.js
@@ -2,18 +2,22 @@ const data = require("./httpserver/data.json");
 
 //mapping
 var names = data.map((n) => n.name);
+
 console.log(names);
 
 const joined = names.join(" ");
+
 console.log(joined);
 
 //filter method
 var user = data.filter((x) => x.id == 1);
+
 console.log(user);
 
 //reduce method
 const array = [1, 2, 3, 4, 5];
 var reduced = array.reduce((accumulator, currentval) => {
-  return accumulator + currentval;
+   return accumulator + currentval;
 }, 0);
+
 console.log(reduced);
--- Week2/promise.js
+++ Week2/promise.js
@@ -1,19 +1,19 @@
-var bool = true;
+var bool = false;
 
 const promise = new Promise((resolve, reject) => {
-  if (bool) {
-    resolve("accepted.");
-  } else {
-    reject("err occured");
-  }
+   if (bool) {
+      resolve("accepted.");
+   } else {
+      reject("err occured");
+   }
 });
 
 promise
-  .then((data) => {
-    console.log(data);
-    console.log("fulfilled.");
-  })
-  .catch((err) => {
-    console.log(err);
-    console.log("failed");
-  });
+   .then((data) => {
+      console.log(data);
+      console.log("fulfilled.");
+   })
+   .catch((err) => {
+      console.log(err);
+      console.log("failed");
+   });
